f26179be8b1b8cf93969cc472acf2828491d493f
Екатерина <catrit1993@gmail.com>
2024-01-22 17:20:47 +0500

    add descriptions

--- classes.py
+++ classes.py
@@ -5,6 +5,9 @@ import os
 
 
 class API(ABC):
+    """
+    Абстрактный класс для подключения к API сайтов
+    """
 
     @abstractmethod
     def get_vacancies(self, keyword, quantity):
@@ -12,35 +15,40 @@ class API(ABC):
 
 
 class HeadHunterAPI(API):
-
+    """
+    Класс для подключения к сайту hh.ru и поиска там вакансий
+    """
     def get_vacancies(self, keyword, quantity):
         url_hh = 'https://api.hh.ru/vacancies'
         vacancies_hh = requests.get(url_hh, params={'text': keyword, 'currency': 'RUR', 'host': 'hh.ru'}).json()
         vacancy = {}
         for i in range(0, quantity):
             try:
-                vac = {
+                vacan = {
                     'name': vacancies_hh['items'][i]['name'],
                     'payment_to': vacancies_hh['items'][i]['salary']['to'],
                     'payment_from': vacancies_hh['items'][i]['salary']['from'],
                     'town': vacancies_hh['items'][i]['area']['name'],
                     'requirement': vacancies_hh['items'][i]['snippet']['requirement']
                 }
+            except IndexError:
+                print('По ключевому слову не найдено вакансий')
             except TypeError:
-                vac = {
+                vacan = {
                     'name': vacancies_hh['items'][i]['name'],
                     'payment_to': 0,
                     'payment_from': 0,
                     'town': vacancies_hh['items'][i]['area']['name'],
                     'requirement': vacancies_hh['items'][i]['snippet']['requirement']
                 }
-            except IndexError:
-                print('По ключевому слову не найдено вакансий')
-            vacancy[vacancies_hh['items'][i]['id']] = vac
+            vacancy[vacancies_hh['items'][i]['id']] = vacan
         return vacancy
 
 
 class SuperJobAPI(API):
+    """
+    Класс для подключения к сайту superjob.ru и поиска там вакансий
+    """
 
     def get_vacancies(self, keyword, quantity):
         secret_key = {'X-Api-App-Id': os.getenv('SJ-API-KEY')}
@@ -49,29 +57,31 @@ class SuperJobAPI(API):
         vacancy = {}
         for i in range(0, quantity):
             try:
-                vac = {
+                vacan = {
                     'name': vacancies_sj['objects'][i]['profession'],
                     'payment_to': vacancies_sj['objects'][i]['payment_to'],
                     'payment_from': vacancies_sj['objects'][i]['payment_from'],
                     'town': vacancies_sj['objects'][i]['town']['title'],
                     'requirement': vacancies_sj['objects'][i]['candidat']
                 }
+            except IndexError:
+                print('По ключевому слову не найдено вакансий')
             except TypeError:
-                vac = {
+                vacan = {
                     'name': vacancies_sj['objects'][i]['profession'],
                     'payment_to': 0,
                     'payment_from': 0,
                     'town': vacancies_sj['objects'][i]['town']['title'],
                     'requirement': vacancies_sj['objects'][i]['candidat']
                 }
-            except IndexError:
-                print('По ключевому слову не найдено вакансий')
-            vacancy[vacancies_sj['objects'][i]['id']] = vac
+            vacancy[vacancies_sj['objects'][i]['id']] = vacan
         return vacancy
 
 
 class Vacancy:
-
+    """
+    Класс для работы с вакансией
+    """
     def __init__(self, name=None, payment_from=0, payment_to=0, town=None, requirement=None):
         self.name = name
         try:
@@ -101,31 +111,59 @@ class Vacancy:
 
 
 class JSONSave(ABC):
-
+    """
+    Абстрактный класс для сохранения данных в json-файл
+    """
     @abstractmethod
     def to_json(self, getted_vac):
+        """
+        Функция для первичного сохранения в пустой файл
+        :param getted_vac: словарь из вакансий
+        :return: None
+        """
         pass
 
     @abstractmethod
     def add_vacancy(self, getted_vac):
+        """
+        Функция для добавления вакансий в уже заполненный файл
+        :param getted_vac: словарь из вакансий
+        :return: None
+        """
         pass
 
     @abstractmethod
     def delete_vacancy(self, getted_vac):
+        """
+        Функция для удаления вакансии из файла
+        :param getted_vac: экземпляр класса Vacancy
+        :return: None
+        """
         pass
 
 
 class JSONSaver(JSONSave):
-
+    """
+    Класс для сохранения данных в json-файл
+    """
     def __init__(self, file='saved_vacancies.json'):
         self.file = file
 
-
     def to_json(self, getted_vac):
+        """
+        Функция для первичного сохранения в пустой файл
+        :param getted_vac: словарь из вакансий
+        :return: None
+        """
         with open(os.path.abspath(self.file), 'w', encoding='utf=8') as f:
             json.dump(getted_vac, f, ensure_ascii=False, indent=2)
 
     def add_vacancy(self, getted_vac):
+        """
+        Функция для добавления вакансий в уже заполненный файл
+        :param getted_vac: словарь из вакансий
+        :return: None
+        """
         getted_vac = json.dumps(getted_vac)
         getted_vac = json.loads(str(getted_vac))
         b = json.load(open(os.path.abspath(self.file), 'r', encoding='utf=8'))
@@ -136,21 +174,26 @@ class JSONSaver(JSONSave):
     def read_vacancy(self):
         b = json.load(open(os.path.abspath(self.file), 'r', encoding='utf=8'))
         vacancies_list = []
-        for key,item in b.items():
-            vacancies_list.append(Vacancy(item['name'], int(item['payment_to']), int(item['payment_from']), item['town'], item['requirement']))
+        for key, item in b.items():
+            vacancies_list.append(Vacancy(item['name'],
+                                          int(item['payment_to']),
+                                          int(item['payment_from']),
+                                          item['town'],
+                                          item['requirement']))
         return vacancies_list
 
     def delete_vacancy(self, del_vac):
-        try:
-            del_vac = json.dumps(del_vac.__dict__)
-            del_vac = json.loads(str(del_vac))
-            name = del_vac['name']
-            payment_from = del_vac['town']
-        except AttributeError:
-            name = del_vac['name']
-            payment_from = del_vac['town']
+        """
+        Функция для удаления вакансии из файла
+        :param del_vac: экземпляр класса Vacancy
+        :return: None
+        """
+        del_vac = json.dumps(del_vac.__dict__)
+        del_vac = json.loads(str(del_vac))
+        name = del_vac['name']
+        town = del_vac['town']
         b = json.load(open(os.path.abspath(self.file), 'r', encoding='utf=8'))
         for key, items in list(b.items()):
-            if items['name'] == name and items['town'] == payment_from:
+            if items['name'] == name and items['town'] == town:
                 b.pop(key, items)
         json.dump(b, open(os.path.abspath(self.file), 'w', encoding='utf=8'), ensure_ascii=False, indent=2)
--- main.py
+++ main.py
@@ -5,10 +5,10 @@ vac = user_interaction()
 json_saver = JSONSaver()
 json_saver.to_json(vac)
 print('Найденные вакансии сохранены в файл')
-com = 0
-while com != 5:
-    com = choise_command()
-    if com == 1:
+command = 0
+while command != 5:
+    command = choise_command()
+    if command == 1:
         vac2 = user_interaction()
         second_search = input('Дозаписать найденные вакансии в файл? Да - 1')
         if int(second_search) == 1:
@@ -16,23 +16,23 @@ while com != 5:
             print('Файл обновлен')
         else:
             continue
-    elif com == 2:
+    elif command == 2:
         a = json_saver.read_vacancy()
         for i in a:
             print(i)
             print('-' * 50)
-    elif com == 3:
+    elif command == 3:
         a = json_saver.read_vacancy()
         for i in sorted(a, reverse=True):
             print(i)
             print('-' * 50)
-    elif com == 4:
+    elif command == 4:
         name = input('Введите название вакансии для удаления:')
         town = input('Введите город, в котором открыта вакансия:')
         del_vac = Vacancy(name, 0, 0, town)
         json_saver.delete_vacancy(del_vac)
         print('Вакансия удалена')
-    elif com not in [1, 2, 3, 4, 5]:
+    elif command not in [1, 2, 3, 4, 5]:
         print('Такая команда не найдена')
 else:
     print('Всего доброго!')
